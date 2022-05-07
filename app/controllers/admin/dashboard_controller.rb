22b26a6a338e92d8457545054213ebe3caa934a3
Maxwell Veer <maxaveer@gmail.com>
2022-05-07 23:39:20 +0000

    added counter to the admin dashboarrd, currently counts: products and categories"

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => "Jungle", :password => "book" 
-  def show
+  def show    
+    @count = { 
+      products: Product.all.size ,
+      categories: Category.all.size
+    }
   end
 end
